d7641aa9aabeba1894ebaa344005b84c4521e6f9
bala-amypo <bala@amypo.in>
2025-12-20 16:24:49 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/DelayScoreServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.DelayScoreService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DelayScoreServiceImpl implements DelayScoreService {
+
+    @Override
+    public void calculateDelayScore() {
+        // logic
+    }
+}
