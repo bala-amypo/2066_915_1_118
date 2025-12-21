96bc3619a18b7d644225747be9789020003bca15
bala-amypo <bala@amypo.in>
2025-12-21 03:33:42 +0000

    User commit

--- src/main/java/com/example/demo/service/DeliveryRecordService.java
+++ src/main/java/com/example/demo/service/DeliveryRecordService.java
@@ -1,28 +1,9 @@
-package com.example.demo.service.impl;
+package com.example.demo.service;
 
 import com.example.demo.model.DeliveryRecord;
-import com.example.demo.repository.DeliveryRecordRepository;
-import com.example.demo.service.DeliveryRecordService;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class DeliveryRecordServiceImpl implements DeliveryRecordService {
-
-    private final DeliveryRecordRepository deliveryRepository;
-
-    public DeliveryRecordServiceImpl(DeliveryRecordRepository deliveryRepository) {
-        this.deliveryRepository = deliveryRepository;
-    }
-
-    @Override
-    public DeliveryRecord recordDelivery(DeliveryRecord record) {
-        return deliveryRepository.save(record);
-    }
-
-    @Override
-    public List<DeliveryRecord> getByPurchaseOrderId(Long purchaseOrderId) {
-        return deliveryRepository.findByPurchaseOrderId(purchaseOrderId);
-    }
+public interface DeliveryRecordService {
+    DeliveryRecord recordDelivery(DeliveryRecord record);
+    List<DeliveryRecord> getByPurchaseOrderId(Long purchaseOrderId);
 }
