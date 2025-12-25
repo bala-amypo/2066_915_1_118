8ac38c4e55d1419f3b79bad7843952f44a7ec055
bala-amypo <bala@amypo.in>
2025-12-25 14:10:14 +0000

    User commit

---
+++ src/main/java/com/example/demo/dto/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.example.demo.dto;
+
+public class LoginRequest {
+
+    private String username;
+    private String password;
+
+    public LoginRequest() {}
+
+    // getters & setters
+}
---
+++ src/main/java/com/example/demo/dto/RegisterRequest.java
@@ -0,0 +1,15 @@
+package com.example.demo.dto;
+
+import com.example.demo.model.Role;
+
+public class RegisterRequest {
+
+    private String username;
+    private String email;
+    private String password;
+    private Role role;
+
+    public RegisterRequest() {}
+
+    // getters & setters
+}
