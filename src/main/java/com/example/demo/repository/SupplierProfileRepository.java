750dd1ac1bee2cae790252d3f4cb1e726d8f7670
bala-amypo <bala@amypo.in>
2025-12-26 03:36:08 +0000

    User commit

--- src/main/java/com/example/demo/repository/SupplierProfileRepository.java
+++ src/main/java/com/example/demo/repository/SupplierProfileRepository.java
@@ -1,19 +1,9 @@
-package com.example.demo.service;
+package com.example.demo.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.demo.model.SupplierProfile;
-
 import java.util.List;
-import java.util.Optional;
 
-public interface SupplierProfileService {
-
-    SupplierProfile createSupplier(SupplierProfile supplierProfile);
-
-    SupplierProfile updateSupplier(Long id, SupplierProfile supplierProfile);
-
-    List<SupplierProfile> getAllSuppliers();
-
-    Optional<SupplierProfile> getSupplierById(Long id);
-
-    void deleteSupplier(Long id);
+public interface SupplierProfileRepository extends JpaRepository<SupplierProfile, Long> {
+    List<SupplierProfile> findByActive(Boolean active);
 }
