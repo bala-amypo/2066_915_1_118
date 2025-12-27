5738f6fb6f72f0f3ff0b8bed85c06f3f1f25dd83
bala-amypo <bala@amypo.in>
2025-12-27 09:35:43 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/SupplierProfileServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/SupplierProfileServiceImpl.java
@@ -1,6 +1,6 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.entity.SupplierProfile;
+import com.example.demo.model.SupplierProfile;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.SupplierProfileRepository;
 import com.example.demo.service.SupplierProfileService;
