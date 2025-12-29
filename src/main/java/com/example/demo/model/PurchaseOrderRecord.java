631e1a597c88bf981481ac8232f57f1b1426077c
bala-amypo <bala@amypo.in>
2025-12-29 05:58:00 +0000

    User commit

--- src/main/java/com/example/demo/model/PurchaseOrderRecord.java
+++ src/main/java/com/example/demo/model/PurchaseOrderRecord.java
@@ -18,7 +18,7 @@ public class PurchaseOrderRecord {
     private LocalDate promisedDeliveryDate;
     private LocalDate issuedDate;
 
-    // getters & setters
+    
     public Long getId() { return id; }
     public void setId(Long id) { this.id = id; }
 
